16f9e9d327904ef762eab19677de5c0735828982
Flo :) <103675778+FlorenceGayton@users.noreply.github.com>
2025-11-06 11:41:29 +0000

    Refactor star drawing into Star class
    Refactored star drawing functionality into a Star class and removed the standalone star function.

--- exercise1-1/sketch.js
+++ exercise1-1/sketch.js
@@ -1,22 +1,46 @@
+class Star {
+    x;
+    y;
+    speed;
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+    draw(){
+        fill(255, 234, 0);
+        noStroke();
+        triangle(this.x, this.y - 50, this.x - 20, this.y, this.x + 20, this.y);
+        triangle(this.x - 50, this.y - 20, this.x, this.y - 20, this.x, this.y + 10);
+        triangle(this.x + 50, this.y - 20, this.x, this.y - 20, this.x, this.y + 10);
+        triangle(this.x - 20, this.y - 5, this.x, this.y + 10, this.x - 35, this.y + 30);
+        triangle(this.x, this.y + 10, this.x + 20, this.y - 5, this.x + 35, this.y + 30);
+    }
+    fall(speed){
+        this.y = this.y + speed;
+    }
+    shoot(speed){
+        this.x = this.x + speed;
+    }
+
+}
+let myStar1 = new Star(100, 300);
+let myStar2 = new Star(200, 100);
+
 function setup() {
     createCanvas(400, 400);
 }
 
 function draw() {
     background(0);
+    myStar1.draw();
+    myStar2.draw();
+    myStar1.fall(2);
+    myStar2.shoot(5);
 }
 
 /**
  * Draws a star at the given coordinates.
- * @param {number} x 
+ * @param {number} this.x 
  * @param {number} y 
+ * @param {number} speed
  */
-function star(x, y) {
-    fill(255, 234, 0);
-    noStroke();
-    triangle(x, y - 50, x - 20, y, x + 20, y);
-    triangle(x - 50, y - 20, x, y - 20, x, y + 10);
-    triangle(x + 50, y - 20, x, y - 20, x, y + 10);
-    triangle(x - 20, y - 5, x, y + 10, x - 35, y + 30);
-    triangle(x, y + 10, x + 20, y - 5, x + 35, y + 30);
-}
